06f6a9f251f8d2e340566806559f7382042ebc8c
신희성 <vvwv3092@gmail.com>
2025-05-27 15:03:14 +0900

    9658 short

--- src/baekjoon/b_9658/Main.java
+++ src/baekjoon/b_9658/Main.java
@@ -7,27 +7,13 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        // SK -> true
-        // CY -> false
-
         boolean[] dp = new boolean[1000+1];
 
-        dp[1] = false;
         dp[2] = true;
-        dp[3] = false;
         dp[4] = true;
 
-        if (n < 5) {
-            System.out.println(dp[n] ? "SK" : "CY");
-            return;
-        }
-
         for (int i = 5; i <= n; i++) {
-            if (!dp[i-1] || !dp[i-3] || !dp[i-4]) {
-                dp[i] = true;
-            } else {
-                dp[i] = false;
-            }
+            dp[i] = !dp[i-1] || !dp[i-3] || !dp[i-4];
         }
 
         System.out.println(dp[n] ? "SK" : "CY");
